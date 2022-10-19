f8f8100b4822c22b62460cd5420f6916a2e166d8
MuhammedKAldin <54613628+MuhammedKAldin@users.noreply.github.com>
2022-10-19 16:07:00 +0200

    Update responsive.js

--- src/responsive.js
+++ src/responsive.js
@@ -1,7 +1,7 @@
 import { css } from "styled-components";
 
 export const mobile = (props) => {
-    return `
+    return css`
       @media only screen and (max-width: 380px) {
         ${props}
       }
